f473d3bb645f4201c2ea58294f7e1870d6e04b3e
martina.isakovic <martina.isakovic@AMS-GYM-EDV1-15.gym.ams.local>
2019-11-04 18:49:50 +0100

    VariablenBasics

---
+++ src/javaBasics/VariablenBasics.java
@@ -0,0 +1,37 @@
+package javaBasics;
+
+public class VariablenBasics {
+
+	public static void main(String[] args) {
+		boolean wahr_oder_falsch;
+		
+		wahr_oder_falsch=true;
+		System.out.println(wahr_oder_falsch);
+		
+		wahr_oder_falsch=false;
+		System.out.println(wahr_oder_falsch);
+		
+		byte zahl = 1;
+		System.out.println(zahl);
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+				
+		
+		short ganz_kleine_zahl = 120;
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		
+		int kleine_zahl = 123456;
+		System.out.println(kleine_zahl);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		
+				
+				
+		
+		
+
+	}
+
+}
