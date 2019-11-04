915e3eb16d56241a9592047a2872097f4e33b834
martina.isakovic <martina.isakovic@AMS-GYM-EDV1-15.gym.ams.local>
2019-11-04 18:17:12 +0100

    JavaBasics

---
+++ src/javaBasics/JavaBasics.java
@@ -0,0 +1,29 @@
+package javaBasics;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hallo");
+		System.out.print(" ");
+		System.out.print("du");
+		System.out.print(" ");
+		System.out.println("!");
+		
+		System.out.println("Hallo" + " " + "du" + " " + "!");
+		
+		System.out.println("120");
+		System.out.println("5");
+		
+		System.out.println("123.45");
+		
+
+		
+		System.out.println(2+2);
+		System.out.println(4-3);
+		System.out.println(3*5);
+		
+		
+	}
+
+}
