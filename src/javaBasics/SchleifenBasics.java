211353afeb11ef31fde2d8cb3951bd5d651afe75
martina.isakovic <martina.isakovic@AMS-GYM-EDV1-15.gym.ams.local>
2019-11-11 18:19:54 +0100

    VariablenBasics

---
+++ src/javaBasics/SchleifenBasics.java
@@ -0,0 +1,25 @@
+package javaBasics;
+
+public class SchleifenBasics {
+
+	public static void main(String[] args) {
+		
+		// FOR Schleife
+		for (int i = 0; i < 5; i = i+1)
+			System.out.println(i);
+		
+		System.out.println("-----");
+		int j =1;
+		for (;j<10;) {
+			System.out.println(j);
+		j = j +1;
+	}
+		for (;3<4;)
+			System.out.println("HuiHui");
+			
+		
+
+		
+	}
+
+}
