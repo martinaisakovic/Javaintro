830d4b6c4152650ce1e0e8680e1176183ff5394f
ACER <ACER@192.168.0.17>
2019-11-17 20:09:31 +0100

    javaBasics

--- src/javaBasics/EinMalEins.java
+++ src/javaBasics/EinMalEins.java
@@ -1,19 +1,10 @@
-public class Einmaleins
-{
-  public static void main( String[] args)
-  {
-    for( int i = 1; i <= 10; i++)
-    {
-      for( int j = 1; j <= 10; j++)
-      {
-                                             // Abstand
-        if( i * j < 10) System.out.print( " ");
-        if( i * j < 100) System.out.print( " ");
-                                             // Produkt
-        System.out.print( " " + i * j);
-      }
-                                       // Zeilenwechsel
-      System.out.println( "");
-    }
-  }
+class KleinesEinMalEins {
+	public static void main (String args[]){
+		int x = 1;
+		
+		for(int i=1;i<=10;i = i + 1) { 
+			for (int j=1;j<10;j = j + 1)
+				System.out.println(i + "x" + j + "=" + i*j); 
+		}
+	}
 }
