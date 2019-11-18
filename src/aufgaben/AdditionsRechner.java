ea7c8f0273cf8f896c701456dd1a9c031aa9d21c
martina.isakovic <martina.isakovic@AMS-GYM-EDV1-15.gym.ams.local>
2019-11-18 18:49:40 +0100

    java5

---
+++ src/aufgaben/AdditionsRechner.java
@@ -0,0 +1,14 @@
+package aufgaben;
+
+import java.util.Scanner;
+
+public class AdditionsRechner {
+
+	public static void main(String[] args) {
+		Scanner eingabe = new Scanner (System.in);
+		System.out.println(eingabe.nextLine());
+
+		
+	}
+
+}
